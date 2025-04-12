deb05326658d338917d6a2078194edfafc256584
Manju362 <pathumchinthaka398@gmail.com>
2025-04-12 09:27:13 +0530

    Update config.js

--- config.js
+++ config.js
@@ -6,7 +6,7 @@ function convertToBool(text, fault = "true") {
   return text === fault ? true : false;
 }
 module.exports = {
-  SESSION_ID: process.env.SESSION_ID || "fawGWQjK#1wpeJzlmFqcaFSsTJg3xq5rCuSNmu31Z3oSfoMAZBck",
+  SESSION_ID: process.env.SESSION_ID || "2PJmSbZa#LSoVewqnJuId6guhMBSX_1dhXL9dUgSaI0a-_R9joq0",
   MONGODB: process.env.MONGODB || "mongodb://mongo:FgDzFEHtnfVtptNXifWYtYTMbPnAJGKK@mainline.proxy.rlwy.net:47582",
   OWNER_NUM: process.env.OWNER_NUM || "94766863255",
   API_KEY:process.env.API_KEY || "sky|e6ad5555ee53b73644770beab633855c2f646a77",
